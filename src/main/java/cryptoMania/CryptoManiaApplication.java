d417418e3af8787b3733eda73d72edcfe27cd750
Mkhululi Ndlovu <87315888+mk870@users.noreply.github.com>
2022-06-12 17:32:03 +0200

    Update CryptoManiaApplication.java

--- src/main/java/cryptoMania/CryptoManiaApplication.java
+++ src/main/java/cryptoMania/CryptoManiaApplication.java
@@ -2,6 +2,8 @@ package cryptoMania;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CryptoManiaApplication {
@@ -9,5 +11,9 @@ public class CryptoManiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoManiaApplication.class, args);
 	}
-
+		@Bean
+		public WebClient.Builder getWebClient()
+		{
+		return WebClient.builder();
+		}
 }
